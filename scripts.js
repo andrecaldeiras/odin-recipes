f290092bb240b55bafcdd1b1f6c5e4333e6555fa
andrecaldeiras <andremedia02@gmail.com>
2023-08-18 14:41:34 +0100

    Spacing and indentation correction on index.html and added recipe page styling with correction of mouse movement effect

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const pos = document.documentElement;
+
 pos.addEventListener("mousemove", (e) => {
-  pos.style.setProperty("--x", e.clientX + "px");
-  pos.style.setProperty("--y", e.clientY + "px");
+    pos.style.setProperty("--x", e.pageX + "px");
+    pos.style.setProperty("--y", e.pageY + "px");
 });
