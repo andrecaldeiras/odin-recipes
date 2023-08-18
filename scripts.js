e6ab027deeb4d83c7b7864abd573e97e87c29971
andrecaldeiras <andremedia02@gmail.com>
2023-08-18 15:22:24 +0100

    Cursor clipping now only turns on if javascript is enabled

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const pos = document.documentElement;
+const circle = document.querySelector(".circle");
+
+circle.classList.add("circle--on");
 
 pos.addEventListener("mousemove", (e) => {
     pos.style.setProperty("--x", e.pageX + "px");
